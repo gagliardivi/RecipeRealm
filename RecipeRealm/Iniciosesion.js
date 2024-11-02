843d0b76a99eaa4bab9daa8f9bcabe6f69eadeeb
nicosavo <ngsavo@gmail.com>
2024-11-02 20:33:11 -0300

    Inicio de sesion corregido

--- RecipeRealm/Iniciosesion.js
+++ RecipeRealm/Iniciosesion.js
@@ -1,37 +1,30 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    
     event.preventDefault();
 
-    
     const usuarioIngresado = document.getElementById('usuario').value.trim();
     const passwordIngresada = document.getElementById('password').value;
 
-    
     const registros = localStorage.getItem('registros');
     if (!registros) {
         alert('No se han registrado usuarios.');
         return;
     }
 
-   
     const usuariosRegistrados = JSON.parse(registros);
 
-    
-    const usuarioValido = Object.values(usuariosRegistrados).find(user => 
-        user.usuario === usuarioIngresado && user.contraseña === passwordIngresada
+    // Buscar el usuario y validar la contraseña
+    const usuarioValido = usuariosRegistrados.usuarios.find(user => 
+        user.usuario === usuarioIngresado && user.password === passwordIngresada
     );
 
     if (usuarioValido) {
-        
-        if (usuarioValido.esAdmin) {
-            
+        // Redirigir según el rol del usuario
+        if (usuarioValido.rol === 'admin') {
             window.location.href = 'admin.html';
         } else {
-            
             window.location.href = 'Home.html';
         }
     } else {
-        
         alert('Usuario o contraseña incorrectos.');
     }
 });
